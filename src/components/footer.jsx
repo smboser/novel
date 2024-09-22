6df8f51479a8f3cf01b46485f69f8dece6efdd78
sm-boser <sm.boser@gmail.com>
2024-09-22 15:00:27 +0530

    change footer copyright

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,11 @@
 export const Footer = () => {
-    return ( 
-        <footer className="bg-info text-center fixed-bottom footer">Powered by novelaquatech</footer>
-    );
+  return (
+    <>
+      {/*// TODO: Need to enable when save mode is ON*/}
+      {/* <footer className="bg-info text-center fixed-bottom footer">Powered by novelaquatech</footer> */}
+      <footer className="bg-info text-center fixed-bottom footer">
+        Powered by Anixsoft
+      </footer>
+    </>
+  );
 };
